59a0e75c48079106af3349e21fb65f55b41ee2d9
web <146587835+ar-bh@users.noreply.github.com>
2026-07-22 15:48:11 -0400

    loop/wave/clone progress bar + label

---
+++ gameplay/gameplay.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+#region node variables
+@onready var player: Player = %Player
+
+@onready var loop_timer: Timer = %LoopTimer
+@onready var loop_label: RichTextLabel = %LoopLabel
+@onready var loop_progress_bar: ProgressBar = %LoopProgressBar
+#endregion
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	loop_timer.timeout.connect(_on_loop_timer_timeout)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	loop_label.text = "[center]Time until next wave/clone: " + str(int(ceil(loop_timer.time_left))) + "s[/center]"
+	loop_progress_bar.value = (loop_timer.time_left / loop_timer.wait_time) * 100.0
+
+func _on_loop_timer_timeout() -> void:
+	pass
