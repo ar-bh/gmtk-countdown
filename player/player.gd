8bc9eed89db7799779996e7b346e8aceac6139ee
web <146587835+ar-bh@users.noreply.github.com>
2026-07-22 14:27:42 -0400

    player movement, bullet and weapon scene, and temporary cotl map
    first commit hooray

---
+++ player/player.gd
@@ -0,0 +1,13 @@
+extends CharacterBody2D
+
+#region movement variables
+@export var speed := 460.0
+@export var ground_friction_factor := 10.0
+#endregion
+
+func _physics_process(delta: float) -> void:
+	var move_direction := Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	var desired_velocity := speed * move_direction
+	var steering := desired_velocity - velocity
+	velocity += steering * ground_friction_factor * delta
+	move_and_slide()
